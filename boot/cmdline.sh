f08397f8ecc8db1dc01484564e9c28ed8d2baaf0
Node Panda <nodepanda@users.noreply.github.com>
2019-10-25 11:13:59 +0200

    Update cmdline.sh

--- boot/cmdline.sh
+++ boot/cmdline.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "rw cryptdevice=UUID=$(blkid /dev/sda2 -o value -s UUID):root root=/dev/mapper/root quiet loglevel=0 rd.systemd.show_status=false rd.udev.log_priority=0"
+CMDLINE="rw cryptdevice=UUID=$(blkid /dev/sda2 -o value -s UUID):root root=/dev/mapper/root quiet loglevel=0 rd.systemd.show_status=false rd.udev.log_priority=0"
+echo "Generating CMDLINE:"
+echo $CMDLINE
